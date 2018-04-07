855bc74fb3c6a61d4ad3b4ae1a38c514638ad573
Tarun Mittal <trmittal24@gmail.com>
2018-04-07 23:50:07 +0530

    2ndseg

--- prac/1counter.cmd
+++ prac/1counter.cmd
@@ -4,10 +4,13 @@ l or2v0x3_1/vss an2v0x3_3/vss
 h iv1v0x3_0/a an2v0x3_0/a REM both are up/down signals
 h tf_0/xor2v0x05_0/a      REM high for the first ff
 
-clock tf_2/dfnt1v0x2_0/cp 0 1
+
+vector en tf_2/dfnt1v0x2_0/cp
+set i 0
+every 50 {setvector en	 0d$i ; d ; incr i}
 relax l
-analyzer tf_2/dfnt1v0x2_0/cp tf_2/dfnt1v0x2_0/z an2v0x3_3/b an2v0x3_0/b
+analyzer tf_2/dfnt1v0x2_0/cp tf_2/dfnt1v0x2_0/z an2v0x3_3/a an2v0x3_0/b or2v0x3_0/z an2v0x3_3/b an2v0x3_2/b
 settle 20
-c 5
- relax l
-c 10
+s 125
+relax l
+s 1000
