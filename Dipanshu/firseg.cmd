87c7bad99cb56066817b4e3f10465ddf4b09cd61
Dipanshu <prasad.dipanshu.prasad@gmail.com>
2018-04-07 11:54:34 +0530

    done with the inside part

---
+++ Dipanshu/firseg.cmd
@@ -0,0 +1,13 @@
+relax l
+h decoder_0/vdd
+l decoder_0/gnd
+l decoder_0/d0
+l decoder_0/d1
+l decoder_0/d2
+l decoder_0/d3
+l decoder_0/d4
+l decoder_0/d5
+h decoder_0/d6
+w out 3_bitmux_0/o0 3_bitmux_0/o1 3_bitmux_0/o2
+s
+
