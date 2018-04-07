5e3692b688f7d42d2956e9beedbd436689dae565
Dipanshu <prasad.dipanshu.prasad@gmail.com>
2018-04-07 13:35:18 +0530

    logic added

--- Dipanshu/firseg.cmd
+++ Dipanshu/firseg.cmd
@@ -1,13 +1,9 @@
 relax l
-h decoder_0/vdd
-l decoder_0/gnd
-l decoder_0/d0
-l decoder_0/d1
-l decoder_0/d2
-l decoder_0/d3
-l decoder_0/d4
-l decoder_0/d5
-h decoder_0/d6
-w out 3_bitmux_0/o0 3_bitmux_0/o1 3_bitmux_0/o2
+h vdd
+l gnd
+h a
+h b
+h c
+w out
 s
 
