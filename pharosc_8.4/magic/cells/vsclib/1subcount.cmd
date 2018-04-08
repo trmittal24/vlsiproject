e97909166d1641f1efbe8ec3df08a4e854d5a450
Tarun Mittal <trmittal24@gmail.com>
2018-04-08 15:58:07 +0530

    ok

--- pharosc_8.4/magic/cells/vsclib/1subcount.cmd
+++ pharosc_8.4/magic/cells/vsclib/1subcount.cmd
@@ -1,22 +1,34 @@
-h an2v0x3_0/vdd 1counter_0/an2v0x3_2/vdd totdiff3_0/mux_0/vdd
-l an2v0x3_0/vss 1counter_0/an2v0x3_3/vss totdiff3_0/mux_0/gnd
+h an2v0x3_0/vdd 1counter_0/an2v0x3_2/vdd totdiff3_0/mux_0/vdd bf1v0x3_0/vdd
+l an2v0x3_0/vss 1counter_0/an2v0x3_3/vss totdiff3_0/mux_0/gnd bf1v0x4_0/vss
 
 relax l
-settle 30ns
+settle 50ns
+h 1counter_0/tf_0/xor2v0x05_0/a
 
-vector en an2v0x3_0/b 
-set i 0
-every 50 {setvector en	 0d$i ; d ; incr i}
-stepsize 150ns
+clock an2v0x3_0/b 0 1
 
-h totdiff3_0/diff2_0/in_b totdiff3_0/diff2_2/in_b
-l totdiff3_0/diff2_1/in_b 
+analyzer  an2v0x3_0/b an2v0x3_0/z or3v0x3_0/a or3v0x3_0/b or3v0x3_0/c xxx bf1v0x3_0/a 1counter_0/bf1v0x4_0/z 1counter_0/tf_2/dfnt1v0x2_0/d 1counter_0/or2v0x3_1/a 1counter_0/an2v0x3_2/z 1counter_0/or2v0x3_1/z bf1v0x4_0/z
 
-analyzer an2v0x3_0/b or3v0x3_0/a or3v0x3_0/b or3v0x3_0/c 1counter_0/an2v0x3_3/a 1counter_0/an2v0x3_0/b
 
-s 1000
+
+ 
+h totdiff3_0/diff2_1/in_b totdiff3_0/diff2_0/in_b
+l totdiff3_0/diff2_2/in_b
+
+c 10
+
+l totdiff3_0/diff2_0/in_b totdiff3_0/diff2_2/in_b
+h totdiff3_0/diff2_1/in_b 
+
+
+c 10
 
 l totdiff3_0/diff2_1/in_b totdiff3_0/diff2_2/in_b
 h totdiff3_0/diff2_0/in_b 
 
-s 1000
+c 10
+
+
+l totdiff3_0/diff2_2/in_b totdiff3_0/diff2_0/in_b
+l totdiff3_0/diff2_1/in_b 
+c 10
