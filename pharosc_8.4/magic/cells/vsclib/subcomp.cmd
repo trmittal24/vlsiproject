f22d3485edc1d67000817276a770162eafa9d4f6
bka_hackman <barunkumaracharya@hotmail.com>
2018-04-08 12:59:51 +0530

    subtractor assembled with comparator

---
+++ pharosc_8.4/magic/cells/vsclib/subcomp.cmd
@@ -0,0 +1,12 @@
+stepsize 50
+h totdiff3_0_mux_0_vdd
+l totdiff3_0_mux_0_gnd
+w comp_0_iv1v0x4_1_a comp_0_iv1v0x4_2_a comp_0_iv1v0x4_0_a comp_0_an3v0x2_2_a  comp_0_an3v0x2_0_b comp_0_an3v0x2_2_c  comp_0_nd3v0x2_0_z
+
+h totdiff3_1_diff2_0_in_a totdiff3_1_diff2_2_in_a totdiff3_1_diff2_0_in_b totdiff3_1_diff2_1_in_b totdiff3_1_diff2_2_in_b
+l totdiff3_1_diff2_1_in_a
+
+l totdiff3_0_diff2_1_in_a totdiff3_0_diff2_2_in_a totdiff3_0_diff2_0_in_b
+h totdiff3_0_diff2_0_in_a totdiff3_0_diff2_1_in_b totdiff3_0_diff2_2_in_b
+
+s
